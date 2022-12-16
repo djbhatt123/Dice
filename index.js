7b5fbc1a40ee867f1a84d6253e92314767f35d8b
djbhatt123 <89781814+djbhatt123@users.noreply.github.com>
2022-12-16 13:01:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var randomNumber1= Math.floor((Math.random()*6))+1;
 
-var randomImageSource1="images/dice"+randomNumber1+".png";
+var randomImageSource1="dice"+randomNumber1+".png";
 
 document.querySelector(".dice .img1").setAttribute("src", randomImageSource1);
 
 var randomNumber2= Math.floor((Math.random()*6))+1;
 
-var randomImageSource2="images/dice"+randomNumber2+".png";
+var randomImageSource2="dice"+randomNumber2+".png";
 
 document.querySelector(".dice .img2").setAttribute("src", randomImageSource2);
 
